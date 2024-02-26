e47fa274ce8530ce58f442942802f72258851cc6
Arda İlhan <ardailhan7@gmail.com>
2024-02-26 11:17:38 +0300

    EventStoreService configs for adding new products

--- Product.Event.Handler.Service/Program.cs
+++ Product.Event.Handler.Service/Program.cs
@@ -1,10 +1,12 @@
 using Product.Event.Handler.Service.Services;
+using Shared.Services;
 using Shared.Services.Abstractions;
 
 var builder = Host.CreateApplicationBuilder(args);
 //builder.Services.AddHostedService<Worker>();
 
 builder.Services.AddSingleton<IEventStoreService, IEventStoreService>();
+builder.Services.AddSingleton<IMongoDBService, MongoDBService>();
 
 var host = builder.Build();
 host.Run();
--- Product.Event.Handler.Service/Services/EventStoreService.cs
+++ Product.Event.Handler.Service/Services/EventStoreService.cs
@@ -1,5 +1,5 @@
-﻿
-using EventStore.Client;
+﻿using EventStore.Client;
+using MongoDB.Driver;
 using Shared.Events;
 using Shared.Services.Abstractions;
 using System.Reflection;
@@ -7,7 +7,7 @@ using System.Text.Json;
 
 namespace Product.Event.Handler.Service.Services
 {
-    public class EventStoreService(IEventStoreService eventStoreService) : BackgroundService
+    public class EventStoreService(IEventStoreService eventStoreService, IMongoDBService mongoDBService) : BackgroundService
     {
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
@@ -17,9 +17,21 @@ namespace Product.Event.Handler.Service.Services
                 object @event = JsonSerializer.Deserialize(resolvedEvent.Event.Data.ToArray(),
                     Assembly.Load("Shared").GetTypes().FirstOrDefault(t => t.Name == eventType));
 
+                var productCollection = mongoDBService.GetCollection<Models.Product>("Products");
+
                 switch (@event)
                 {
                     case NewProductAddedEvent e:
+                        var hasProduct = await (await productCollection.FindAsync(p => p.Id == e.ProductId)).AnyAsync();
+                        if (!hasProduct)
+                            await productCollection.InsertOneAsync(new()
+                            {
+                                Id = e.ProductId,
+                                ProductName = e.ProductName,
+                                Count = e.InitialCount,
+                                IsAvailable = e.IsAvailable,
+                                Price = e.InitialPrice
+                            });
 
                         break;
                 }
